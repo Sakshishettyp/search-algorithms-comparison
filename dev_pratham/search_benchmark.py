3d45e293b37b9a52a0bb9765ee878ad9ddcafd0d
Pratham S <pratham@tetrisinnovations.com>
2026-01-23 16:39:16 +0530

    Fix spelling typos

--- dev_pratham/search_benchmark.py
+++ dev_pratham/search_benchmark.py
@@ -1,5 +1,5 @@
-#Liner Search
-def liner_count(arr, x):
+#Linear Search
+def linear_count(arr, x):
     count = 0
     for i in arr:
         if i == x:
@@ -11,7 +11,7 @@ def liner_count(arr, x):
 
 import bisect
 
-#Binay Search
+#Binary Search
 def binary_count(arr, x):
     first = bisect.bisect_left(arr, x)
     if first == len(arr) or arr[first] != x:
@@ -31,19 +31,19 @@ def benchmark():
         arr = sorted([random.randint(0, size // 2) for _ in range(size)])
 
         start = time.perf_counter()
-        liner_count(arr, size // 2)
-        liner_time = time.perf_counter() - start
+        linear_count(arr, size // 2)
+        linear_time = time.perf_counter() - start
 
         start = time.perf_counter()
         binary_count(arr, size // 2)
         binary_time = time.perf_counter() - start
 
-        print(f"Size: {size}, liner_time: {liner_time:10.10f}, binary_time: {binary_time:10.10f}")
+        print(f"Size: {size}, linear_time: {linear_time:10.10f}, binary_time: {binary_time:10.10f}")
 
 
 a = [1, 1, 2, 4, 5, 5, 7, 9]
 x = 5
-print("Linear Count value: ",liner_count(a, x))
+print("Linear Count value: ",linear_count(a, x))
 print("Binary Count value: ",binary_count(a, x))
 
 benchmark()
@@ -53,11 +53,11 @@ benchmark()
 Output:
 Linear Count value:  2
 Binary Count value:  2
-Size: 10, liner_time: 0.0000009000, binary_time: 0.0000011000
-Size: 100, liner_time: 0.0000029000, binary_time: 0.0000011000
-Size: 1000, liner_time: 0.0000230001, binary_time: 0.0000010999
-Size: 10000, liner_time: 0.0002488000, binary_time: 0.0000017000
-Size: 100000, liner_time: 0.0033508000, binary_time: 0.0000056999
-Size: 1000000, liner_time: 0.1047675000, binary_time: 0.0000071999
-Size: 10000000, liner_time: 1.3514788999, binary_time: 0.0000097000
+Size: 10, linear_time: 0.0000009000, binary_time: 0.0000015000
+Size: 100, linear_time: 0.0000030000, binary_time: 0.0000009000
+Size: 1000, linear_time: 0.0000242001, binary_time: 0.0000008000
+Size: 10000, linear_time: 0.0002309000, binary_time: 0.0000018000
+Size: 100000, linear_time: 0.0028625999, binary_time: 0.0000055000
+Size: 1000000, linear_time: 0.1148440000, binary_time: 0.0000077999
+Size: 10000000, linear_time: 1.3171556999, binary_time: 0.0000083001
 '''
